b62cc9cd523df520c4a38a21dcae7ed6a59cdfca
Forta <aezelvalfcarpenterdp@gmail.com>
2025-04-25 18:08:25 +0700

    Update population_data.py

--- population_data.py
+++ population_data.py
@@ -1,6 +1,6 @@
 # population_data
 import pandas as pd
-
+data_population = {'Caralina'}
 data = {
     'State': ['California', 'Texas', 'Florida', 'New York', 'Pennsylvania', 'Illinois', 'Ohio', 'Georgia', 'North Carolina', 'Michigan'],
     'Population': [39538223, 29145505, 21538187, 20201249, 13002700, 12812508, 11799448, 10711908, 10439388, 10077331]
